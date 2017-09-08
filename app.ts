83c56040e82ab9cd2df7bc7268f30f310d721c1f
Pokhodyun Alexander <karbunkul@yourtask.ru>
2017-09-08 11:31:19 +0500

    fix gitignore

--- app.ts
+++ app.ts
@@ -7,16 +7,20 @@ const sbi = new SBI();
 
 sbi.set({key: 'config', value: {
     NODE_ENV: conf.NODE_ENV || 'dev',
-    TEST: conf.TEST,
+    TEST: conf.TEST || 'dev',
 }});
 const multi = sbi.set({key: 'models'})
     .add('schedule', 'scheduleSchema')
     .add('user', 'userSchema');
 
-console.log(multi.item('user'));
+// console.log(multi.item('user'));
+//
+// console.log(sbi.boxes());
+// console.log(sbi.keys());
+//
+// const config = sbi.get('config').item();
+// console.log(SBI.newInstance().get(''));
 
-console.log(sbi.boxes());
-console.log(sbi.keys());
+console.log(SBI.newInstance().get('models').item('user'));
 
-const config = sbi.get('config').item();
-console.log(SBI.newInstance().get(''));
+console.log(SBI.newInstance().get('config').item());
